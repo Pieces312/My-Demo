a9f42eb2c77cb3df4a53b27546a079dcc9139fac
Alex <1143034433@qq.com>
2018-12-10 12:51:40 +0800

    update

---
+++ vue-login/src/components/login/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="login-box">
+        <div class="bgImg"><img src="@/assets/beauty.jpg" alt="beauty" /></div>
+        <div class="form">
+            <login></login>
+        </div>
+    </div>
+</template>
+
+<script>
+import Login from './login.vue';
+
+export default {
+    name: 'form-box',
+    components: {Login}
+}
+</script>
+
+<style lang="less">
+@import '~css/login.less';
+</style>
---
+++ vue-login/src/components/login/login.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="inner-box">
+        <div class="form-header">Login</div>
+        <form class="form-body">
+            <div class="row-input">
+                <input v-model='formData.acount' type="text" placeholder="手机号/邮箱">
+                <i class="icon-close"></i>
+            </div>
+            <div class="row-input">
+                <input v-model='formData.password' type="password" placeholder="密码">
+                <i class="icon-close"></i>
+            </div>
+            <div class="btn-row">
+                <button class="btn">Login</button>
+            </div>
+        </form>
+        <div class="form-footer">
+            <button class="btn">Don't have account?</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'login-box',
+    data() {
+        return {
+            formData: {
+                acount: '',
+                password: ''
+            }
+        }
+    },
+    mounted() {
+        
+    },
+    methods: {
+        inputValue(e, val) {
+            console.log(e);
+        }
+    }
+}
+</script>
